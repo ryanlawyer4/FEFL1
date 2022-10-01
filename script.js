5ddce210392cd1f594613bcfda77137581a7da31
ryanlawyer4 <112990972+ryanlawyer4@users.noreply.github.com>
2022-10-01 17:06:56 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function numberChecking(number1, number2){
+    if(number1>100){
+        console.log("Number 1 has many digits!");
+    }
+    if(number1<0){
+        console.log("Number 1 is a negative number!");
+    }
+    if(number2<20){
+        console.log("Number 2 is not a very high number!");
+    }
+    if(number2>50){
+        console.log("Number 2 is a very high number!");
+    }
+   
+}
+var number1=145;
+var number2=18;
+numberChecking(number1, number2)
